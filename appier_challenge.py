7fd24042627d8adb421705425553fbd55df5ff27
x95102003 <x95102003@gmail.com>
2017-03-19 21:14:18 +0800

    Add binding port_env.

--- appier_challenge.py
+++ appier_challenge.py
@@ -48,5 +48,6 @@ if __name__ == "__main__":
     backcrawl = threading.Thread(target=UBM.update_num)
     backcrawl.daemon = True
     backcrawl.start()
-    app.run(host='0.0.0.0', port=1234)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
